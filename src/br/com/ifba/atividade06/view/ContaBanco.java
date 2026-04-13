114312357f63f13d8a1d29c6a46fb666a8aefdcf
ariianesfranca@gmail.com <ariia@ArianeF>
2026-04-13 19:34:38 -0300

    Atividade 06 - Criação dos métodos da classe ContaBanco

---
+++ src/br/com/ifba/atividade06/view/ContaBanco.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade06.view;
+
+/**
+ *
+ * @author ariia
+ */
+public final class ContaBanco {
+    //Criando atributos
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+    //Métodos personalizados 
+    public void abrirConta(){
+    }
+    public void fecharConta(){
+        
+    }
+    public void depositar(){
+    }
+    public void sacar(){
+    }
+    public void pagarMensal(){
+    }
+    //Metodos especiais 
+   
+    public ContaBanco() {
+        this.setSaldo(0);
+        this.setStatus(false);
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+   
+    
+    
+}
