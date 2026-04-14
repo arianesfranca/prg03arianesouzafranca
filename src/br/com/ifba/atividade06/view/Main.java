e4b7dda25908bf8727ec1eceb30dcb7ccf1fa3e4
ariianesfranca@gmail.com <ariia@ArianeF>
2026-04-13 21:13:41 -0300

    Cria classe main com teste inicial de conta banco

--- src/br/com/ifba/atividade06/view/Main.java
+++ src/br/com/ifba/atividade06/view/Main.java
@@ -9,5 +9,12 @@ package br.com.ifba.atividade06.view;
  * @author ariia
  */
 public class Main {
-    
+   public static void main(String[] args) {
+       ContaBanco p1 = new ContaBanco ();
+       p1.setNumConta(1111);
+       p1.setDono("Jubileu");
+       p1.abrirConta("CC");
+       p1.estadoAtual();
+       
+   }
 }
