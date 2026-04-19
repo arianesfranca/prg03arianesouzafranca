dfbcef1ac0b5b38921d5b7bf4789834ca35f1842
ariianesfranca@gmail.com <ariia@ArianeF>
2026-04-19 16:40:37 -0300

    cria classe Sessao com atributos, construtores, getters, setters e toString

---
+++ src/br/com/ifba/atividade08/view/Sessao.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade08.view;
+
+/**
+ *
+ * @author ariia
+ */
+public class Sessao {
+
+    Sessao(Object object, Usuario aThis, String token) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+  private Long id;
+  private Usuario usuario;
+  private String token;
+  
+  public Sessao(){}
+  
+  public Sessao(Long id, Usuario usuario, String token){
+      this.id = id;
+      this.usuario =  usuario;
+      this.token = token;
+      
+  }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String toString() {
+        return "Sessao{" +
+               "id=" + id +
+               ", usuario=" + usuario.getNomeUsuario() +
+               ", token='" + token + '\'' +
+               '}';
+    }
+  
+}
