f02684205e0a74c3502cc2fb82a8f573086165e8
ariianesfranca@gmail.com <ariia@ArianeF>
2026-04-20 18:51:11 -0300

    adiciona classe abstrata base Forma

---
+++ src/br/com/ifba/atividade09/view/Forma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade09.view;
+
+/**
+ *
+ * @author ariia
+ */
+public abstract class Forma {
+
+    @Override
+    public abstract String toString();
+    
+}
