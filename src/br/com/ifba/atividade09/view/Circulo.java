e37fab93fa3584f00f7dffbc53c5d66918292304
ariianesfranca@gmail.com <ariia@ArianeF>
2026-04-20 19:54:50 -0300

    adiciona classes concretas Circulo, Quadrado e Triangulo

---
+++ src/br/com/ifba/atividade09/view/Circulo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade09.view;
+
+/**
+ *
+ * @author ariia
+ */
+public class Circulo extends Forma2D {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+    @Override
+    public double obterArea() {
+      return Math.PI * raio * raio;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo com raio" + raio;
+    }
+}
---
+++ src/br/com/ifba/atividade09/view/Quadrado.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade09.view;
+
+/**
+ *
+ * @author ariia
+ */
+public class Quadrado extends Forma2D {
+    private double lado;
+    
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double obterArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Quadrado com lado" + lado;
+    }
+    
+}
